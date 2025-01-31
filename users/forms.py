63216f0412fefcd3343b301ca7c1e0619e6ebae1
Joaquin Palacio <joaquinpalaciofeijoo.04@gmail.com>
2025-01-31 17:12:13 -0300

    Se elimina forms.py debido a que queda en desuso

--- users/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
-
-
-class UpdateProfileForm(UserChangeForm):
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'base_currency']
