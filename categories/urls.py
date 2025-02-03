821485e1410373c153e8dc30e5275cd1de77fa02
Joaquin Palacio <joaquinpalaciofeijoo.04@gmail.com>
2025-02-03 01:41:17 -0300

    Creacion del CRUD de categories

--- categories/urls.py
+++ categories/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_categories, name='categories'),
-    path('create/', views.create, name='create'),
-    path('<id>', views.detail, name='detail'),
-    path('<id>/edit/', views.edit, name='edit'),
-    path('<id>/delete/', views.delete, name='delete'),
+    path('<id>', views.detail, name='detail_category'),
+    path('<id>/edit/', views.edit, name='edit_category'),
+    path('<id>/delete/', views.delete, name='delete_category'),
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,23 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from .models import Category
 
 # Create your views here.
 
-
+@login_required
 def all_categories(request):
-    return render(request, 'categories.html')
+    user = request.user
+    categories = Category.objects.filter(user=user)
+    if request.method == 'POST':
+        name = request.POST['name']
+        if not name:
+            messages.error(request, 'Name is required.')
+            return render(request, 'categories.html', {'categories': categories})
+        
+        if not (Category.objects.filter(name__iexact=name, user=user).exists()):
+            Category.objects.create(name=name, user=user)
+            return render(request, 'categories.html', {'categories': categories})
+        
+        messages.error(request, 'Category already exists.')
+
+    return render(request, 'categories.html', {'categories': categories})
 
 
-def create(request):
-    return render(request, 'create.html')
+@login_required
+def detail(request, id):
+    category = get_object_or_404(Category, id=id)
+    return render(request, 'detail_category.html', {'category': category})
 
 
-def detail(request):
-    return render(request, 'detail.html')
+@login_required
+def edit(request, id):
+    category = get_object_or_404(Category, id=id)
+    if request.method == 'POST':
+        name = request.POST['name']
+        category.name = name
+        category.save()
+        return redirect('detail_category', id=category.id)
+    return render(request, 'edit_category.html', {'category': category})
 
 
-def edit(request):
-    return render(request, 'edit.html')
-
-
-def delete(request):
-    return render(request, 'delete.html')
+@login_required
+def delete(request, id):
+    category = get_object_or_404(Category, id=id)
+    category.delete()
+    return redirect('categories')
