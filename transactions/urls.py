775e3f1765a184a1244debb7c1825f956fddc37c
Joaquin Palacio <joaquinpalaciofeijoo.04@gmail.com>
2025-02-03 19:21:41 -0300

    Creacion de CRUD de transactions

--- transactions/urls.py
+++ transactions/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.transactions, name='transactions'),
-    path('<id>', views.detail, name='detail'),
-    path('create/', views.create, name='create'),
-    path('<id>/edit/', views.edit, name='edit'),
-    path('<id>/delete/', views.delete, name='delete'),
+    path('<id>/edit/', views.edit, name='edit_transaction'),
+    path('<id>/delete/', views.delete, name='delete_transaction'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Transaction
 from .forms import TrasactionForm
+from categories.models import Category
+from currencies.models import Currency
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,47 +12,51 @@ from .forms import TrasactionForm
 @login_required
 def transactions(request):
     transactions = Transaction.objects.filter(user=request.user)
-    return render(request, 'transactions.html', {'transactions': transactions})
-
-
-@login_required
-def detail(request, id):
-    transaction = get_object_or_404(Transaction, id=id, user=request.user)
-    return render(request, 'detail.html', {'transaction': transaction})
-
-
-@login_required
-def create(request):
+    categories = Category.objects.filter(user=request.user)
+    currencies = Currency.objects.all()
     if request.method == 'POST':
-        form = TrasactionForm(request.POST, user=request.user)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.user = request.user
-            transaction.save()
+        amount = request.POST['amount']
+        type_transaction = request.POST['type_transaction']
+        category_id = request.POST['category']
+        currency_id = request.POST['currency']
+        
+        try:
+            category = Category.objects.get(id=category_id)
+            currency = Currency.objects.get(id=currency_id)
+            Transaction.objects.create(amount=amount, type_transaction=type_transaction, category=category, currency=currency, user=request.user)
             return redirect('transactions')
-    else:
-        form = TrasactionForm(user=request.user)
-    return render(request, 'create.html', {'form': form})
+        except Exception as e:
+            messages.error(request, f"Error al crear la transacción: {e}")
+    return render(request, 'transactions.html', {'transactions': transactions, 'categories': categories, 'currencies': currencies})
 
 
 @login_required
 def edit(request, id):
-    transaction = get_object_or_404(Transaction, id=id, user=request.user)
+    categories = Category.objects.filter(user=request.user)
+    currencies = Currency.objects.all()
+    transaction = get_object_or_404(Transaction, id=id)
     if request.method == 'POST':
-        form = TrasactionForm(
-            request.POST, instance=transaction, user=request.user)
-        if form.is_valid():
-            form.save()
+        amount = request.POST['amount']
+        type_transaction = request.POST['type_transaction']
+        category_id = request.POST['category']
+        currency_id = request.POST['currency']
+        try:
+            category = Category.objects.get(id=category_id)
+            currency = Currency.objects.get(id=currency_id)
+            transaction.amount = amount
+            transaction.type_transaction = type_transaction
+            transaction.category = category
+            transaction.currency = currency
+            transaction.save()
             return redirect('transactions')
-    else:
-        form = TrasactionForm(instance=transaction, user=request.user)
-    return render(request, 'edit.html', {'form': form})
+        except Exception as e:
+            messages.error(request, f"Error al crear la transacción: {e}")
+
+    return render(request, 'edit_transaction.html', {'transaction': transaction, 'categories': categories, 'currencies': currencies})
 
 
 @login_required
 def delete(request, id):
     transaction = get_object_or_404(Transaction, id=id, user=request.user)
-    if request.method == 'POST':
-        transaction.delete()
-        return redirect('transactions')
-    return render(request, 'delete.html', {'transaction': transaction})
+    transaction.delete()
+    return redirect('transactions')
